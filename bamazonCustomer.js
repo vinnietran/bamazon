ebbce16c18a279ca0dd7188eefbfd7871229d566
VinTran <tranquillo.vinnie@gmail.com>
2019-04-08 15:05:02 -0400

    initial

---
+++ bamazonCustomer.js
@@ -0,0 +1,17 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "",
+  database: "bamazon"
+});
